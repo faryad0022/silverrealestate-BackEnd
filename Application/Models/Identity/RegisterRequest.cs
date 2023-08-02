ddd9bd33e0a363f231747aa9390087253c9baa64
Administrator <admin@example.com>
2023-08-02 18:01:07 +0330

    add models for identity

---
+++ Application/Models/Identity/RegisterRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Models.Identity
+{
+    public class RegisterRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; }
+
+    }
+}
---
+++ Application/Models/Identity/RegisterResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Models.Identity
+{
+    public class RegisterResponse
+    {
+        public string UserId { get; set; }
+    }
+}
